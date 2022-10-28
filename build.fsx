6929a8a1d30e25b8d2c1216509b0a416ac6c40f4
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2022-10-28 16:48:34 +0800

    Update build

--- build.fsx
+++ build.fsx
@@ -3,13 +3,13 @@
 #r "nuget: Fake.Core.Environment,5.20.0"
 #r "nuget: Fake.IO.Zip,5.20.0"
 #r "nuget: BlackFox.Fake.BuildTask,0.1.3"
-#r "nuget: Fun.Build, 0.2.2"
+#r "nuget: Fun.Result"
+#r "nuget: Fun.Build, 0.2.3"
 
 open System
 open System.IO
-open System.Diagnostics
+open Fun.Result
 open Fun.Build
-open Fake.Core
 open Fake.IO
 open Fake.IO.Globbing.Operators
 open Fake.IO.FileSystemOperators
@@ -18,6 +18,23 @@ open Fake.IO.FileSystemOperators
 let serverPath = __SOURCE_DIRECTORY__ </> "Slaveoftime.Site"
 let deployDir = __SOURCE_DIRECTORY__ </> "deploy"
 let publishDir = deployDir </> "publish"
+
+
+type StageContext with
+    member ctx.MSDeploy(zippedPackageFile: string) = asyncResult {
+        let host = ctx.GetEnvVar "IIS_SERVER_COMPUTER_NAME"
+        let user = ctx.GetEnvVar "IIS_SERVER_USERNAME"
+        let pwd = ctx.GetEnvVar "IIS_SERVER_PASSWORD"
+        let appName = ctx.GetEnvVar "RECYCLE_APP_NAME"
+        let siteName = ctx.GetEnvVar "IIS_WEBSITE_NAME"
+        let targetDir = ctx.GetEnvVar "WEBSITE_CONTENT_PATH"
+
+        let msdeploy (x: string) = ctx.RunCommand($"'C:/Program Files (x86)/IIS/Microsoft Web Deploy V3/msdeploy.exe' {x}")
+
+        do! msdeploy $"-verb:sync -allowUntrusted -source:recycleApp -dest:recycleApp=\"{appName}\",recycleMode=\"StopAppPool\",computerName=\"{host}/msdeploy.axd?site={siteName}\",username=\"{user}\",password=\"{pwd}\",AuthType=\"Basic\""
+        do! msdeploy $"-verb:sync -allowUntrusted -source:package=\"{zippedPackageFile}\" -dest:contentPath=\"{targetDir}\",computerName=\"{host}/msdeploy.axd?site={siteName}\",username=\"{user}\",password=\"{pwd}\",AuthType=\"Basic\" -enableRule:DoNotDeleteRule"
+        do! msdeploy $"-verb:sync -allowUntrusted -source:recycleApp -dest:recycleApp=\"{appName}\",recycleMode=\"StartAppPool\",computerName=\"{host}/msdeploy.axd?site={siteName}\",username=\"{user}\",password=\"{pwd}\",AuthType=\"Basic\""
+    }
 
 
 let checkEnv =
@@ -56,29 +73,15 @@ pipeline "deploy" {
     }
     stage "push" {
         whenEnvVar "GITHUB_ACTION"
-        run (fun ctx ->
+        run (fun ctx -> asyncResult {
             let packagePath = deployDir </> DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".zip"
 
             if Directory.Exists deployDir |> not then Shell.mkdir deployDir
 
             !!(publishDir </> "**/*.*") |> Zip.zip publishDir packagePath
 
-            let host = ctx.GetEnvVar "IIS_SERVER_COMPUTER_NAME"
-            let user = ctx.GetEnvVar "IIS_SERVER_USERNAME"
-            let pwd = ctx.GetEnvVar "IIS_SERVER_PASSWORD"
-            let appName = ctx.GetEnvVar "RECYCLE_APP_NAME"
-            let siteName = ctx.GetEnvVar "IIS_WEBSITE_NAME"
-            let targetDir = ctx.GetEnvVar "WEBSITE_CONTENT_PATH"
-
-            let msdeploy (x: string) =
-                let cmd = ProcessStartInfo("C:/Program Files (x86)/IIS/Microsoft Web Deploy V3/msdeploy.exe", Arguments = x)
-                let proc = Process.Start cmd
-                proc.WaitForExit()
-
-            msdeploy $"-verb:sync -allowUntrusted -source:recycleApp -dest:recycleApp=\"{appName}\",recycleMode=\"StopAppPool\",computerName=\"{host}/msdeploy.axd?site={siteName}\",username=\"{user}\",password=\"{pwd}\",AuthType=\"Basic\""
-            msdeploy $"-verb:sync -allowUntrusted -source:package=\"{packagePath}\" -dest:contentPath=\"{targetDir}\",computerName=\"{host}/msdeploy.axd?site={siteName}\",username=\"{user}\",password=\"{pwd}\",AuthType=\"Basic\" -enableRule:DoNotDeleteRule"
-            msdeploy $"-verb:sync -allowUntrusted -source:recycleApp -dest:recycleApp=\"{appName}\",recycleMode=\"StartAppPool\",computerName=\"{host}/msdeploy.axd?site={siteName}\",username=\"{user}\",password=\"{pwd}\",AuthType=\"Basic\""
-        )
+            do! ctx.MSDeploy packagePath
+        })
     }
     runIfOnlySpecified
 }
